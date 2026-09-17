7fef53bea475be87c3ea2bd2e3c847a7650c025e
s-chuch <shawnchucheong@hotmail.com>
2026-09-17 11:52:37 -0400

    Refresh FPL data 2026-09-17T15:35Z

--- data.js
+++ data.js
@@ -1,11 +1,1 @@
-window.FPL_DATA = {
-  "transfers": [
-    {
-      "gw": 3,
-      "out": "Mbeumo",
-      "inn": "Cherki",
-      "net": "-5",
-      "verdict": "Lost that GW"
-    }
-  ]
-};
+PLACEHOLDER
